481e5c48894e66bba52df107b50850bca2d14936
Santi <santimaldonadoochoa@gmail.com>
2021-02-25 18:33:07 -0300

    Update main.py

--- main.py
+++ main.py
@@ -46,8 +46,8 @@ volumen = SimulationVolume(voxelSize=voxelSize, N=N)
 #microestructuras
 medidas = [0.128,0.256,0.256]
 
-muestra = Muestra(volumen, medidas=medidas, geometria='distancia_constante', ancho=16e-3, distancia=20e-3)
-# muestra = Muestra(volumen, medidas=medidas, geometria='cilindritos_aleatorios_2',ancho=16e-3, distancia=20e-3) # para 'porcentaje_palos'
+# muestra = Muestra(volumen, medidas=medidas, geometria='distancia_constante', ancho=16e-3, distancia=20e-3)
+# muestra = Muestra(volumen, medidas=medidas, geometria='cilindritos_aleatorios_2',ancho=16e-3, distancia=20e-3) 
 #%% CREACION DEL OBJETO DELTA--------------------------------------------------
 # delta es la perturbacion de campo magnetico
 delta = Delta(muestra)
