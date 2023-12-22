cfe0dd04aff4299fb05f0cf67139534bbb59474a
santimaldo <santimaldonadoochoa@gmail.com>
2023-12-22 17:02:11 -0300

    Update smc.py

--- DataBases/smc.py
+++ DataBases/smc.py
@@ -74,7 +74,8 @@ T1 = 170e-3
 
 N = 64
 b = 1/14
-r_list = np.linspace(0, int(8/b-1), 1024)
+# r_list = np.linspace(0, int(8/b-1), 1024)
+r_list = np.linspace(0, int(8/b-1), 256)
 # evanesencia: B1 = B10*beta = B10 * exp(-r/12um)
 beta = np.exp(-b*r_list)
 k_list = np.arange(0.5, 3.1, 0.1)
